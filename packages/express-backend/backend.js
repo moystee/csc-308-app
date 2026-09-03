4661cc2961ac5f8a5dd7507ed32352d619ee031a
moystee <jmoy02@calpoly.edu>
2026-09-03 12:42:56 -0700

    Connect frontend to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -60,6 +61,7 @@ const findUsersByNameAndJob = (name, job) =>
     (user) => user["name"] === name && user["job"] === job
   );
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
