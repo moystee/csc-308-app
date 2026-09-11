4a938a620fbac339806455ac7767273e8f82829c
moystee <jmoy02@calpoly.edu>
2026-09-11 14:50:06 -0700

    Complete stock portfolio increment 2.3

--- applying-tdd/stockPortfolio.js
+++ applying-tdd/stockPortfolio.js
@@ -1,6 +1,15 @@
 class StockPortfolio {
+    constructor() { 
+        this.stocks = {}; }
+    isEmpty() { // if the list of ticker names currently stored is empty return true, otherwise false
+        return Object.keys(this.stocks).length === 0; }
 
-    isEmpty() { return true; }
+    // add a number of shares of a stock to the portfolio
+    purchase(symbol, shares) { 
+        if (this.stocks[symbol] === undefined) { // if the stock is not already in the portfolio
+        this.stocks[symbol] = 0; } // set new stock to 0 shares
+
+        this.stocks[symbol] = this.stocks[symbol] + shares; } // add # shares to existing stock
 
 }
 
--- applying-tdd/stockPortfolio.test.js
+++ applying-tdd/stockPortfolio.test.js
@@ -12,3 +12,11 @@ test("new portfolio is empty", () => {
   expect(portfolio.isEmpty()).toBe(true);
 });
 
+test("purchasing shares makes the portfolio not empty", () => {
+  const portfolio = new StockPortfolio();
+
+  portfolio.purchase("RBLX", 5);
+
+  expect(portfolio.isEmpty()).toBe(false);
+});
+
