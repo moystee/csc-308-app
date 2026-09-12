b25e2afa1365e094f3f09dcd0b46f57d334f72ec
moystee <jmoy02@calpoly.edu>
2026-09-11 22:56:41 -0700

    Add TDD reflection

--- applying-tdd/stockPortfolio.test.js
+++ applying-tdd/stockPortfolio.test.js
@@ -84,3 +84,12 @@ test("the portfolio cannot sell more shares than it owns", () => {
 
   expect(() => {portfolio.sell("RBLX", 2); }).toThrow("Not possible to sell this number of shares.");
 });
+
+// 3
+
+// Yes, I was able to successfully follow the test-first approach. This is because this is very familiar to the
+// design recipe I have been learning in other CSC classes and I realize how universal and important it is to
+// first brainstorm and write tests before writing the actual code. Attempting to write the code head on was
+// more difficult because it was difficult to think of both the end result and the functionality to get there.
+// Therefore, the red-gren refractor cycle was very helpful in allowing me to focus on one thing at a time and
+// make sure that I understood what the inputs and outputs would be before the implementation step.
